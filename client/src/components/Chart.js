92c71a2d043ff04a1628cdfa69f90c7b78bd0bb5
Frédéric HELLE <frederic.helle@epitech.eu>
2020-04-20 17:46:58 +0200

    Chart v2

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -4,6 +4,7 @@ import {
   AreaChart, Area, BarChart, Bar
 } from 'recharts';
 import {Card, Checkbox, FormControlLabel, Typography} from '@material-ui/core'
+import moment from 'moment'
 
 import '../styles/Chart.css'
 
@@ -13,13 +14,26 @@ class Chart extends React.Component {
     super(props);
     this.state = {
       activeFrance: false,
+      activeUSA: false,
+      activeSweden: false,
+      activeKorea: false,
       activeWorld: true
     }
+  }
+
+  formatXAxis = tickItem => {
+    return moment(tickItem).format('MMM Do')
   }
 
   handleChangeWorld = e => {
     this.setState({
       activeWorld: !this.state.activeWorld
+    })
+  }
+
+  handleChangeKorea = () => {
+    this.setState({
+      activeKorea: !this.state.activeKorea
     })
   }
 
@@ -29,10 +43,21 @@ class Chart extends React.Component {
     })
   }
 
+  handleChangeUSA = () => {
+    this.setState({
+      activeUSA: !this.state.activeUSA
+    })
+  }
+
+  handleChangeSweden = () => {
+    this.setState({
+      activeSweden: !this.state.activeSweden
+    })
+  }
+
   render() {
 
-    const dataWorld = this.props.data.World
-    const dataFrance = this.props.data.France
+    const globalData = this.props.data
 
     return (
         <div className="ChartContainer">
@@ -42,19 +67,35 @@ class Chart extends React.Component {
                     <LineChart
                         width={500}
                         height={300}
-                        data={dataWorld}
+                        data={globalData}
                         margin={{
                         top: 5, right: 30, left: 20, bottom: 5,
                         }}
                     >
                         <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="date" />
+                        <XAxis dataKey="date" tickFormatter={this.formatXAxis}/>
                         <YAxis />
                         <Tooltip />
                         <Legend />
                         {
                           this.state.activeWorld &&
-                            <Line type="monotone" dataKey="total" stroke="#F6B352" activeDot={{ r: 8 }} />
+                            <Line type="monotone" dataKey="World" stroke="#F6B352" activeDot={{ r: 8 }} />
+                        }                        
+                        {
+                          this.state.activeFrance &&
+                            <Line type="monotone" dataKey="France" stroke='#4077D2' activeDot={{ r: 8 }} />
+                        }
+                        {
+                          this.state.activeUSA &&
+                          <Line type="monotone" dataKey="US" stroke='#DC1F4E' activeDot={{ r: 8 }} />
+                        }
+                        {
+                          this.state.activeSweden &&
+                          <Line type="monotone" dataKey="Sweden" stroke='#43A047' activeDot={{ r: 8 }} />
+                        }
+                        {
+                          this.state.activeKorea &&
+                          <Line type="monotone" dataKey="Korea, South" stroke='#ff5722' activeDot={{ r: 8 }} />
                         }
                     </LineChart>
                 </ResponsiveContainer>
@@ -81,6 +122,42 @@ class Chart extends React.Component {
             />
             }
             label="France"
+            />
+            <FormControlLabel
+            control={
+            <Checkbox
+              checked={this.state.activeUSA}
+              onChange={this.handleChangeUSA}
+              name="checkedB"
+              color="primary"
+              style={{color: '#DC1F4E'}}
+            />
+            }
+            label="USA"
+            />
+            <FormControlLabel
+            control={
+            <Checkbox
+              checked={this.state.activeSweden}
+              onChange={this.handleChangeSweden}
+              name="checkedB"
+              color="primary"
+              style={{color: '#43A047'}}
+            />
+            }
+            label="Sweden"
+            />
+            <FormControlLabel
+            control={
+            <Checkbox
+              checked={this.state.activeKorea}
+              onChange={this.handleChangeKorea}
+              name="checkedB"
+              color="primary"
+              style={{color: '#ff5722'}}
+            />
+            }
+            label='South Korea'
             />
         </div>
     );
