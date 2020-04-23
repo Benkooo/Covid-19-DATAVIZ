f6afc1f08d69a27bf44b919b8a7af605893af078
Frédéric HELLE <frederic.helle@epitech.eu>
2020-04-23 20:50:55 +0200

    fix desktop/mobile button

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
 import logo from '../assets/logo.png'
-import { AppBar, Toolbar, Typography, IconButton, Button} from '@material-ui/core'
+import { AppBar, Toolbar, Typography, IconButton, Button, Popover, MenuList, MenuItem} from '@material-ui/core'
 import MoreIcon from '@material-ui/icons/MoreVert'
 
 class TopBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            toggleMenu: false
+        }
+        this.myRef = React.createRef()
+    }
+
+    activateMenu = () => {
+        this.setState({
+            toggleMenu: !this.state.toggleMenu
+        })
+    }
 
     render() {
         return (
@@ -19,10 +32,27 @@ class TopBar extends React.Component {
                         <Typography variant="h6" style={{fontFamily: 'Product Sans'}}>
                             COVID-19 pandemic real time data
                         </Typography>
-                        <IconButton aria-label="display more actions" edge="end" color="inherit" style={{marginLeft: 'auto'}}>
+                        <IconButton ref={this.myRef} onClick={this.activateMenu} aria-label="display more actions" edge="end" color="inherit" style={{marginLeft: 'auto'}}>
                             <MoreIcon />
                         </IconButton>
-                        <Button onClick={() => this.props.setMobile()} style={{marginLeft: 'auto'}} variant="contained">{this.props.label}</Button>
+                        <Popover
+                            open={this.state.toggleMenu}
+                            onClose={this.activateMenu}
+                            anchorEl={this.myRef.current}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'center'
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                            }}>
+                            <MenuList>
+                        <MenuItem onClick={() => this.props.setMobile()}>{this.props.label}</MenuItem>
+                                <MenuItem>Informations</MenuItem>
+                                <MenuItem>About</MenuItem>
+                            </MenuList>
+                        </Popover>
                     </Toolbar>
                 </AppBar>
             </div>
